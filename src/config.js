08d9fea842ee6cabe906dc050a3734ab1031428d
jruizsilva <ruizsilvajonathan@gmail.com>
2023-06-23 17:22:04 -0300

    .env changed

--- src/config.js
+++ src/config.js
@@ -1,28 +1,20 @@
-import * as dotenv from "dotenv"; // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
-dotenv.config();
+import * as dotenv from 'dotenv' // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
+dotenv.config()
 
-const {
-  LOCAL_DB_USERNAME,
-  LOCAL_DB_PASSWORD,
-  LOCAL_DB_HOST,
-  LOCAL_DB_DATABASE,
-  DEPLOY_DB_USERNAME,
-  DEPLOY_DB_PASSWORD,
-  DEPLOY_DB_HOST,
-  DEPLOY_DB_DATABASE,
-} = process.env;
+const { PGDATABASE, PGHOST, PGPASSWORD, PGPORT, PGUSER } = process.env
 
 module.exports = {
   local: {
-    username: LOCAL_DB_USERNAME || "postgres",
-    password: LOCAL_DB_PASSWORD || "admin",
-    host: LOCAL_DB_HOST || "localhost",
-    database: LOCAL_DB_DATABASE || "database",
+    username: 'postgres',
+    password: 'admin',
+    host: 'localhost',
+    database: 'database'
   },
   deploy: {
-    username: DEPLOY_DB_USERNAME,
-    password: DEPLOY_DB_PASSWORD,
-    host: DEPLOY_DB_HOST,
-    database: DEPLOY_DB_DATABASE,
-  },
-};
+    database: PGDATABASE,
+    username: PGUSER,
+    password: PGPASSWORD,
+    host: PGHOST,
+    port: PGPORT
+  }
+}
