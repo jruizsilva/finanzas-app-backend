96612050e29f331a361b6592a0164986007c2bcf
jruizsilva <ruizsilvajonathan@gmail.com>
2023-10-28 15:17:08 -0300

    refactor db conexion

--- src/database/db.js
+++ src/database/db.js
@@ -1,60 +1,23 @@
 import { Sequelize } from 'sequelize'
-import { local, deploy } from '../config'
 
 const sequelize =
   process.env.NODE_ENV === 'production'
-    ? new Sequelize({
-        database: deploy.database,
-        username: deploy.username,
-        password: deploy.password,
-        host: deploy.host,
-        port: deploy.port,
-        dialect: 'postgres',
-        logging: false,
-        pool: {
-          max: 3,
-          min: 1,
-          idle: 10000
-        },
+    ? new Sequelize(process.env.DATABASE_URL, {
         dialectOptions: {
           ssl: {
-            require: true,
-            rejectUnauthorized: false
-          },
-          keepAlive: true
+            require: true
+          }
         },
-        ssl: true
+        dialect: 'postgres'
       })
     : new Sequelize({
-        database: local.database,
-        username: local.username,
-        password: local.password,
-        host: local.host,
-        port: local.port,
+        username: 'postgres',
+        password: 'admin',
+        host: 'localhost',
+        database: 'finanzas_app',
+        port: 5432,
         dialect: 'postgres',
         logging: false
       })
-
-// const sequelize = new Sequelize({
-//   database: deploy.database,
-//   username: deploy.username,
-//   password: deploy.password,
-//   host: deploy.host,
-//   dialect: 'postgres',
-//   logging: false,
-//   pool: {
-//     max: 3,
-//     min: 1,
-//     idle: 10000
-//   },
-//   dialectOptions: {
-//     ssl: {
-//       require: true,
-//       rejectUnauthorized: false
-//     },
-//     keepAlive: true
-//   },
-//   ssl: true
-// })
 
 export default sequelize
